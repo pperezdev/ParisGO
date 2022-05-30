0db81fbf7c1d972fdcdefd5e42b28c84bc94cb97
pperezdev <pierrick.perez@free.fr>
2022-05-30 19:42:47 +0200

    patch dbsql

--- eks_api.tf
+++ eks_api.tf
@@ -3,7 +3,7 @@ resource "aws_eks_cluster" "api_ekscluster01padd01" {
   role_arn = aws_iam_role.api_eksrole01padd01.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.default_subnet.id]
+    subnet_ids = [aws_default_subnet.default_subnet.id]
   }
 
   depends_on = [
