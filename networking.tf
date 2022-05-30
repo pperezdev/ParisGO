703868197331ffbab4aa0dec0c5370d1990b5092
pperezdev <pierrick.perez@free.fr>
2022-05-30 20:01:57 +0200

    patch dbsql

--- networking.tf
+++ networking.tf
@@ -6,7 +6,7 @@ data "aws_subnet_ids" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc.id
 }
 
-resource "aws_default_subnet" "default_subnet" {
+resource "aws_default_subnet" "main" {
   availability_zone = var.region
   tags = {
     Name = "Main"
