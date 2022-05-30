e8fe243823d44fe5c35834ba9988a5a89703bd9d
pperezdev <pierrick.perez@free.fr>
2022-05-30 20:30:27 +0200

    patch networking 2.0

--- networking.tf
+++ networking.tf
@@ -1,14 +1,18 @@
 data "aws_vpc" "default_vpc" {
   default = true
-  cidr_block = "10.0.1.0/16"
 }
 
 data "aws_subnet_ids" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc.id
 }
 
+resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
+  vpc_id     = "${data.aws_vpc.default_vpc.id}"
+  cidr_block = "10.0.1.0/16"
+}
+
 resource "aws_subnet" "main" {
-  vpc_id     = data.aws_vpc.default_vpc.id
+  vpc_id     = "${aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id}"
   cidr_block = "10.0.1.0/24"
 
   tags = {
