bcded17560c9f098599606544d7fe6544da998a6
pperezdev <pierrick.perez@free.fr>
2022-05-30 18:29:23 +0200

    add EKS patch database

--- networking.tf
+++ networking.tf
@@ -5,3 +5,12 @@ data "aws_vpc" "default_vpc" {
 data "aws_subnet_ids" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc.id
 }
+
+resource "aws_subnet" "default_subnet" {
+  vpc_id     = aws_vpc.default_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
