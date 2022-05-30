e0d228fa899b7a3d1e2841cc59b70e7bac86c776
pperezdev <pierrick.perez@free.fr>
2022-05-30 20:14:22 +0200

    patch networking 2.0

--- networking.tf
+++ networking.tf
@@ -6,8 +6,10 @@ data "aws_subnet_ids" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc.id
 }
 
-resource "aws_default_subnet" "main" {
-  availability_zone = var.region
+resource "aws_subnet" "main" {
+  vpc_id     = "${data.aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
   tags = {
     Name = "Main"
   }
