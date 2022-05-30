af31521ef99638c2205b64a631e6804afafd6277
pperezdev <pierrick.perez@free.fr>
2022-05-30 19:40:28 +0200

    patch dbsql

--- database.tf
+++ database.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "sqldbpa4dd01" {
   allocated_storage   = 20
   storage_type        = "standard"
   engine              = "postgres"
-  engine_version      = "13.4"
+  engine_version      = "13.4-R1"
   instance_class      = "db.t3.micro"
   name                = var.AWS_DATABASE_NAME
   username            = var.AWS_DATABASE_USERNAME
