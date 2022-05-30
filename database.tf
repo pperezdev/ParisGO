cc29e7a4a87aee14b4eec9397f5f21badb3254b8
pperezdev <pierrick.perez@free.fr>
2022-05-30 19:32:08 +0200

    patch networking

--- database.tf
+++ database.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "sqldbpa4dd01" {
   storage_type        = "standard"
   engine              = "postgres"
   engine_version      = "13.4"
-  instance_class      = "db.t4.micro"
+  instance_class      = "db.t3.micro"
   name                = var.AWS_DATABASE_NAME
   username            = var.AWS_DATABASE_USERNAME
   password            = var.AWS_DATABASE_PASSWORD
