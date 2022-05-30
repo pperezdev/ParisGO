8778ed820febc16474644e219b3dc7a2aec963a8
pperezdev <pierrick.perez@free.fr>
2022-05-30 19:34:40 +0200

    patch networking

--- networking.tf
+++ networking.tf
@@ -6,9 +6,9 @@ data "aws_subnet_ids" "default_subnet" {
   vpc_id = data.aws_vpc.default_vpc.id
 }
 
-resource "aws_subnet" "default_subnet" {
+resource "aws_default_subnet" "default_subnet" {
   vpc_id     = data.aws_vpc.default_vpc.id
-  cidr_block = "10.0.1.0/24"
+
   tags = {
     Name = "Main"
   }
