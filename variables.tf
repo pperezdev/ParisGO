a11715439ecab553efbb2a330c21f1988aa09c5c
pperezdev <pierrick.perez@free.fr>
2022-05-30 16:02:10 +0200

    adding variable file

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "acess key of aws"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "aws secret"
+  type        = string
+}
