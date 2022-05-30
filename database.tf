4d4ca64ec979dc9232c40f08303b9fa8ce50140a
pperezdev <pierrick.perez@free.fr>
2022-05-30 18:15:14 +0200

    add EKS patch database

--- database.tf
+++ database.tf
@@ -2,8 +2,8 @@ resource "aws_db_instance" "sqldbpa4dd01" {
   allocated_storage   = 20
   storage_type        = "standard"
   engine              = "postgres"
-  engine_version      = "12.5"
-  instance_class      = "db.t2.micro"
+  engine_version      = "13.4"
+  instance_class      = "db.t4.micro"
   name                = var.AWS_DATABASE_NAME
   username            = var.AWS_DATABASE_USERNAME
   password            = var.AWS_DATABASE_PASSWORD
---
+++ eks_api.tf
@@ -0,0 +1,13 @@
+resource "aws_eks_cluster" "api_ekscluster01padd01" {
+  name     = "api_ekscluster01padd01"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
