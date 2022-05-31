312eaac4ece5d62b709499121ea488c7101a1673
pperezdev <pierrick.perez@free.fr>
2022-05-31 20:46:19 +0200

    modify vpc networking

--- networking.tf
+++ networking.tf
@@ -1,14 +1,14 @@
-data "aws_vpc" "default_vpc" {
+data "aws_vpc" "main" {
   cidr_block = "172.2.0.0/16"
   
 }
 
 data "aws_subnet_ids" "default_subnet" {
-  vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = data.aws_vpc.main.id
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
-  vpc_id     = "${data.aws_vpc.default_vpc.id}"
+  vpc_id     = "${data.aws_vpc.main.id}"
   cidr_block = "172.2.0.0/16"
 }
 
