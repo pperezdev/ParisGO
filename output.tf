31a4bdb446a1fb4e9204c3c6c7e726d4590a9f50
pperezdev <pierrick.perez@free.fr>
2022-05-31 22:13:20 +0200

    networking for EKS patch

--- output.tf
+++
@@ -1,7 +0,0 @@
-output "endpoint" {
-  value = aws_eks_cluster.api_ekscluster01padd01.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.api_ekscluster01padd01.certificate_authority[0].data
-}
