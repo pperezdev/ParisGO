b71d66a24ee25b715170bf21d013092af0bf4947
pperezdev <pierrick.perez@free.fr>
2022-05-31 21:13:16 +0200

    modify vpc networking

--- networking.tf
+++ networking.tf
@@ -1,16 +1,16 @@
 resource "aws_vpc" "main" {
-  cidr_block = "172.2.0.0/16"
+  cidr_block = "10.2.0.0/16"
   instance_tenancy = "default"
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "172.2.0.0/16"
+  cidr_block = "10.2.0.0/16"
 }
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
-  cidr_block = "172.2.0.0/24"
+  cidr_block = "10.2.0.0/24"
 
   tags = {
     Name = "Main"
