ae77a8bc2560baa9927feb7eeca56f1c8212036f
pperezdev <pierrick.perez@free.fr>
2022-05-31 21:22:02 +0200

    add gateway vpc networking

--- networking.tf
+++ networking.tf
@@ -1,16 +1,15 @@
 resource "aws_vpc" "main" {
-  default          = true
-  instance_tenancy = "default"
-}
-
-resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
-  vpc_id     = aws_vpc.main.id
   cidr_block = "10.2.0.0/16"
 }
 
+resource "aws_internet_gateway" "name" {
+  vpc_id = aws_vpc.main.id 
+}
+
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc_ipv4_cidr_block_association.secondary_cidr.vpc_id
+  vpc_id     = aws_vpc.main.vpc_id
   cidr_block = "10.2.0.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Main"
