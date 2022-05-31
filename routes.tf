d29dad81e2c98beb823b8db9a76799b43909554b
pperezdev <pierrick.perez@free.fr>
2022-05-31 22:52:25 +0200

    networking for EKS patch

--- routes.tf
+++ routes.tf
@@ -30,12 +30,11 @@ resource "aws_route_table" "public" {
   route = [
     {
         cidr_block                 = "0.0.0.0/0"
-        gateway_id             = aws_internet_gateway.igw.id
+        gateway_id                 = aws_internet_gateway.igw.id
         nat_gateway_id             = ""
         carrier_gateway_id         = ""
         destination_prefix_list_id = ""
         egress_only_gateway_id     = ""
-        gateway_id                 = ""
         instance_id                = ""
         ipv6_cidr_block            = ""
         local_gateway_id           = ""
