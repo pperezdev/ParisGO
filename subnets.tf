87005f6267d875de0a208b0d2cff0599013cba36
pperezdev <pierrick.perez@free.fr>
2022-07-04 15:37:43 +0200

    modify rds

--- subnets.tf
+++ subnets.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "private-us-east-1a" {
 }
 
 resource "aws_subnet" "default" {
-  vpc_id                  = aws_vpc.default.id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
