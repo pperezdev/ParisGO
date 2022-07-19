ff9f4cad16fa68793a7aef32eb544dc2bac9ff2d
pperezdev <pierrick.perez@free.fr>
2022-07-19 10:39:50 +0200

    add was transfert family patch 6

--- iam-odc.tf
+++ iam-odc.tf
@@ -65,6 +65,6 @@ resource "aws_transfer_user" "ftp" {
   home_directory_type = "LOGICAL"
   home_directory_mappings {
     entry  = "/"
-    target = "/s3pa4dd01/"
+    target = "/s3pa4dd01"
   }
 }
