fcd3e2cc5d5b35ab41cf97091de02e2904c5abcc
pperezdev <pierrick.perez@free.fr>
2022-07-19 09:42:14 +0200

    add was transfert family

--- iam-odc.tf
+++ iam-odc.tf
@@ -7,3 +7,36 @@ resource "aws_iam_openid_connect_provider" "eks" {
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
   url             = aws_eks_cluster.api_ekscluster01padd01.identity[0].oidc[0].issuer
 }
+
+resource "aws_iam_role_policy" "ftp" {
+  name = "tf-test-transfer-user-iam-policy"
+  role = aws_iam_role.ftp.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowFullAccesstoS3",
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_transfer_user" "ftp" {
+  server_id = aws_transfer_server.ftp.id
+  user_name = "tftestuser"
+  role      = aws_iam_role.ftp.arn
+
+  home_directory_type = "LOGICAL"
+  home_directory_mappings {
+    entry  = "/"
+    target = "/bucket3/"
+  }
+}
--- storage.tf
+++ storage.tf
@@ -20,3 +20,16 @@ resource "aws_s3_bucket_object" "refined_folder" {
     key     =  "refined_folder/"
     source  = "/dev/null"
 }
+
+resource "aws_transfer_server" "awstserverpa4dd01" {
+  tags = {
+    Name = "awstserverpa4dd01"
+  }
+}
+
+resource "aws_transfer_access" "awstaccesspa4dd01" {
+  external_id    = "S-1-1-12-1234567890-123456789-1234567890-1234"
+  server_id      = aws_transfer_server.awstserverpa4dd01.id
+  role           = aws_iam_role.ftp.arn
+  home_directory = "/${aws_s3_bucket.s3pa4dd01.id}/"
+}
